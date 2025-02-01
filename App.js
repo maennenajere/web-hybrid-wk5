5f534dd7686315aea1ef3f9ec8186c3c3a2c9030
Jere Mäennenä <jere.maennena@gmail.com>
2025-02-01 23:08:56 +0200

    dondon

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Alert } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [location, setLocation] = useState(null);
+    const [markers, setMarkers] = useState([]);
+
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                Alert.alert("error");
+                return;
+            }
+
+            let userLocation = await Location.getCurrentPositionAsync({});
+            setLocation({
+                latitude: userLocation.coords.latitude,
+                longitude: userLocation.coords.longitude,
+                latitudeDelta: 0.02,
+                longitudeDelta: 0.02,
+            });
+        })();
+    }, []);
+
+    const handleLongPress = (event) => {
+        const newMarker = event.nativeEvent.coordinate;
+        setMarkers([...markers, newMarker]);
+    };
+
+    return (
+        <View style={styles.container}>
+            {location ? (
+                <MapView
+                    style={styles.map}
+                    initialRegion={location}
+                    onLongPress={handleLongPress}
+                    showsUserLocation={true}
+                >
+                    {markers.map((marker, index) => (
+                        <Marker key={index} coordinate={marker} />
+                    ))}
+                </MapView>
+            ) : null}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    map: {
+        flex: 1,
+        width: "100%",
+        height: "100%",
+    },
 });
